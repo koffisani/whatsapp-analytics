7be018756706f6565996e1a4673274071c46d706
koffisani <koffisani@gmail.com>
2020-05-14 14:32:31 +0000

    Added requirements

---
+++ script.py
@@ -0,0 +1,10 @@
+#!venv/usr/bin/python
+
+def count_sent(file):
+    count = 0
+    return count
+
+if __name__ == "__main__":
+    print ("Script d'analyse de discussions WhatsApp")
+    print ("Merci de saisir le nom de la personne ")
+    
